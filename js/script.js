ca65686635af0a4a659ba740b5154b5dd8b270b7
Joshua Adkisson <112263154+Everardos@users.noreply.github.com>
2024-02-13 21:04:23 -0500

    Add operate function

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "plus":
+            return add(num1, num2);
+        case "minus":
+            return subtract(num1, num2);
+        case "times":
+            return multiply(num1, num2);
+        case "over":
+            return divide(num1, num2);
+    }
+}
