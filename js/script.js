722953e905760ebea8d6e9698d3d1023b6ba876e
Joshua Adkisson <112263154+Everardos@users.noreply.github.com>
2024-02-15 23:18:26 -0500

    Add event listeners for operations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+//Display-related variables and functions
+let num1;
+let num2;
+let operator;
+
+let displayValue = "";
+
+function updateDisplay() {
+    displayValue += this.textContent;
+    display.textContent = displayValue;
+}
+
+
 //Calculation functions
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -13,21 +26,9 @@ function operate(num1, num2, operator) {
             return subtract(num1, num2);
         case "times":
             return multiply(num1, num2);
-        case "over":
+        case "divide":
             return divide(num1, num2);
     }
-}
-
-//Display-related variables and functions
-let num1;
-let num2;
-let operator;
-
-let displayValue = "";
-
-function updateDisplay() {
-    displayValue += this.textContent;
-    display.textContent = displayValue;
 }
 
 
@@ -49,3 +50,7 @@ for (i = 0; i < digitsButtons.length; i++) {
     digitsButtons[i].addEventListener("click", updateDisplay);
 }
 
+timesButton.addEventListener("click", setSign);
+divideButton.addEventListener("click", setSign);
+minusButton.addEventListener("click", setSign);
+plusButton.addEventListener("click", setSign);
