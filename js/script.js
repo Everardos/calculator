a36a7133f32b28ca72007bef5604069440c34aaa
Joshua Adkisson <112263154+Everardos@users.noreply.github.com>
2024-02-15 23:03:55 -0500

    Improve structure and comment code

--- js/script.js
+++ js/script.js
@@ -1,33 +1,9 @@
+//Calculation functions
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-let num1;
-let num2;
-let operator;
-
-let displayValue = "";
-
-const digitsButtons = document.querySelectorAll(".digit");
-const display = document.querySelector("#display");
-const timesButton = document.querySelector("#times");
-const divideButton = document.querySelector("#divide");
-const plusButton = document.querySelector("#plus");
-const minusButton = document.querySelector("#minus");
-const equalsButton = document.querySelector("#equals");
-const clearButton = document.querySelector("#clear");
-const backspaceButton = document.querySelector("#backspace");
-const decimalButton = document.querySelector("#decimal");
-
-function logValue() {
-    displayValue += this.textContent;
-    display.textContent = displayValue;
-}
-
-for (i = 0; i < digitsButtons.length; i++) {
-    digitsButtons[i].addEventListener("click", logValue);
-}
 
 function operate(num1, num2, operator) {
     switch (operator) {
@@ -40,4 +16,33 @@ function operate(num1, num2, operator) {
         case "over":
             return divide(num1, num2);
     }
+}
+
+//Display-related variables
+let num1;
+let num2;
+let operator;
+
+let displayValue = "";
+
+//Element selectors
+const digitsButtons = document.querySelectorAll(".digit");
+const display = document.querySelector("#display");
+const timesButton = document.querySelector("#times");
+const divideButton = document.querySelector("#divide");
+const plusButton = document.querySelector("#plus");
+const minusButton = document.querySelector("#minus");
+const equalsButton = document.querySelector("#equals");
+const clearButton = document.querySelector("#clear");
+const backspaceButton = document.querySelector("#backspace");
+const decimalButton = document.querySelector("#decimal");
+
+//Event listeners
+function logValue() {
+    displayValue += this.textContent;
+    display.textContent = displayValue;
+}
+
+for (i = 0; i < digitsButtons.length; i++) {
+    digitsButtons[i].addEventListener("click", logValue);
 }
